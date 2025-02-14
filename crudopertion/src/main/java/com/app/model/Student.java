7421738a6053ba49f7b4916a903537ba7845ade7
payalkhawase <payal.khawase14@gmail.com>
2025-02-14 11:04:12 +0530

    add model package

--- crudopertion/src/main/java/com/app/model/Student.java
+++ crudopertion/src/main/java/com/app/model/Student.java
@@ -1,5 +1,21 @@
 package com.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int rollno;
+	private String name;
+	private String address;
+	private long mobno;
+	private float marks;
 
 }

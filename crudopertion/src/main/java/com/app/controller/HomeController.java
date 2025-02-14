6b813836656f3095319ab82617101bf928824d49
sujata-cell <srjp90@gmail.com>
2025-02-14 11:44:00 +0530

    put mappiing

--- crudopertion/src/main/java/com/app/controller/HomeController.java
+++ crudopertion/src/main/java/com/app/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.model.Student;
 import com.app.servicei.StudentServiceI;
 
-@RestController
+@RestController 
 public class HomeController { 
 
 	@Autowired

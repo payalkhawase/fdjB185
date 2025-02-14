3bf723c13aad28c7e1c5410b9f268699e79caec9
sujata-cell <srjp90@gmail.com>
2025-02-14 11:38:44 +0530

    added put mappingg b sujata

--- crudopertion/src/main/java/com/app/controller/HomeController.java
+++ crudopertion/src/main/java/com/app/controller/HomeController.java
@@ -1,8 +1,14 @@
 package com.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.model.Student;
 import com.app.servicei.StudentServiceI;
 
 @RestController
@@ -10,4 +16,12 @@ public class HomeController {
 
 	@Autowired
 	StudentServiceI ssi;
+	
+	@PutMapping("update/{rollno}")
+	public ResponseEntity<Student> putMethodName(@PathVariable int rollno, @RequestBody Student per) {
+		//TODO: process PUT request
+		Student so = ssi.updateData(rollno,per);
+		
+		return new ResponseEntity<Student>(so,HttpStatus.OK);
+	}
 }
--- crudopertion/src/main/java/com/app/serviceImpl/StudentServiceImpl.java
+++ crudopertion/src/main/java/com/app/serviceImpl/StudentServiceImpl.java
@@ -3,6 +3,7 @@ package com.app.serviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.model.Student;
 import com.app.repository.StudentRepository;
 import com.app.servicei.StudentServiceI;
 
@@ -11,4 +12,10 @@ public class StudentServiceImpl implements StudentServiceI{
 
 	@Autowired
 	StudentRepository sr;
+
+	@Override
+	public Student updateData(int rollno, Student per) {
+		// TODO Auto-generated method stub
+		return sr.save(per);
+	}
 }
